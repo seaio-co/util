adb7b22e546e14857f07ca5297d631a9817c700a
Abby <Abby11991@outlook.com>
2019-07-25 16:02:57 +0800

    optimized code

--- decimal/decimal.go
+++ decimal/decimal.go
@@ -69,7 +69,6 @@ var twentyInt = big.NewInt(20)
 // number = value * 10 ^ exp
 type Decimal struct {
 	value *big.Int
-
 	// NOTE(vadim): this must be an int32, because we cast it to float64 during
 	// calculations. If exp is 64 bit, we might lose precision.
 	// If we cared about being able to represent every possible decimal, we
