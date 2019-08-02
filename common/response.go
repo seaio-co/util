0c7a98350493e905f0cf49e5867f5d53499b9770
wuxiaoyong <wuxiaoyong@blhcn.com>
2019-08-02 10:21:37 +0800

    Cancel the message processing that gin customizes

--- common/response.go
+++ common/response.go
@@ -1,10 +1,5 @@
 package common
 
-import (
-	"github.com/gin-gonic/gin"
-	"strconv"
-)
-
 // SuccessBody 成功响应结构体
 type ResponseBody struct {
 	Status   string `json:"status"`
@@ -26,29 +21,4 @@ func Error(errcode, msg string) ResponseBody {
 		Status:   errcode,
 		ErrorMsg: msg,
 	}
-}
-
-// ReturnSuccessResponse HTTP接口返回数据信息
-func ReturnSuccessResponse(ctx *gin.Context, data interface{}) {
-
-	response := &ResponseBody{
-		Status: "200",
-		Data:   data,
-	}
-
-	ctx.JSON(200, response)
-	return
-}
-
-// ReturnErrorResponse HTTP接口返回错误 //err返回时表示系统出现错误，errMsg返回表示请求无效
-func ReturnErrorResponse(ctx *gin.Context, statusStr, errorMsg string) {
-
-	response := &ResponseBody{
-		Status:   statusStr,
-		ErrorMsg: errorMsg,
-	}
-
-	status, _ := strconv.Atoi(statusStr)
-	ctx.JSON(status, response)
-	return
 }
