0151de1f5b2b52fc4c279c1f545836b32b357edd
Kevin123181 <Kevin123181@outlook.com>
2019-08-20 10:07:57 +0800

    Add reverse a string method and the test go file.

--- stringutil/stringutil.go
+++ stringutil/stringutil.go
@@ -4,6 +4,7 @@ import (
 	"crypto/rand"
 	"encoding/hex"
 	"io"
+	"unicode"
 )
 
 // GetRandString 随机生成N位字符串
@@ -14,4 +15,44 @@ func GetRandString(n int) string {
 		panic("reading from crypto/rand failed: " + err.Error())
 	}
 	return hex.EncodeToString(mainBuff)[:n]
+}
+
+// Reverse reverses the input while respecting UTF8 encoding and combined characters
+func Reverse(text string) string {
+	textRunes := []rune(text)
+	textRunesLength := len(textRunes)
+	if textRunesLength <= 1 {
+		return text
+	}
+
+	i, j := 0, 0
+	for i < textRunesLength && j < textRunesLength {
+		j = i + 1
+		for j < textRunesLength && IsMark(textRunes[j]) {
+			j++
+		}
+
+		if IsMark(textRunes[j-1]) {
+			// Reverses Combined Characters
+			reverse(textRunes[i:j], j-i)
+		}
+
+		i = j
+	}
+
+	// Reverses the entire array
+	reverse(textRunes, textRunesLength)
+
+	return string(textRunes)
+}
+
+func reverse(runes []rune, length int) {
+	for i, j := 0, length-1; i < length/2; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+}
+
+// IsMark determines whether the rune is a marker
+func IsMark(r rune) bool {
+	return unicode.Is(unicode.Mn, r) || unicode.Is(unicode.Me, r) || unicode.Is(unicode.Mc, r)
 }
