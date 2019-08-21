c820afe5c14495750dbe65c4275d3000a60e43f7
Abby-yc <Abby11991@outlook.com>
2019-08-21 09:42:42 +0800

    Add net package and a conv method.

--- conv/conversions.go
+++ conv/conversions.go
@@ -36,3 +36,12 @@ func PrettyJson(data interface{}) (string, error) {
 	}
 	return buffer.String(), nil
 }
+
+// 转换 bool 类型到 int 类型
+// true 转换为 1, false 转换为 0
+func Bool2Int(b bool) int {
+	if b {
+		return 1
+	}
+	return 0
+}
