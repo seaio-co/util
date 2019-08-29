132bb246997c0542c6c0b4306849d5a8bc99fdf8
Abby-yc <Abby11991@outlook.com>
2019-08-29 09:07:13 +0800

    update the json.

--- common/json.go
+++ common/json.go
@@ -2,10 +2,8 @@ package common
 
 import "encoding/json"
 
-// SortedJSON takes any JSON and returns it sorted by keys. Also, all white-spaces
-// are removed.
-// This method can be used to canonicalize JSON to be returned by GetSignBytes,
-// e.g. for the ledger integration.
+// SortedJSON takes any JSON and returns it sorted by keys. Also, all white-spaces are removed.
+// This method can be used to canonicalize JSON to be returned by GetSignBytes,e.g. for the ledger integration.
 // If the passed JSON isn't valid it will return an error.
 func SortJSON(toSortJSON []byte) ([]byte, error) {
 	var c interface{}
