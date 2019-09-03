db57bc1c4800727dc312db6201758eb0a3370cc4
kerryalid <kerryalid@foxmaii.cn>
2019-09-03 09:43:32 +0800

    add int file in the conv package

---
+++ conv/int.go
@@ -0,0 +1,33 @@
+package conv
+
+import (
+	"encoding/binary"
+	"fmt"
+	"reflect"
+)
+
+// convert any numeric value to int64
+func ToInt64(value interface{}) (d int64, err error) {
+	val := reflect.ValueOf(value)
+	switch value.(type) {
+	case int, int8, int16, int32, int64:
+		d = val.Int()
+	case uint, uint8, uint16, uint32, uint64:
+		d = int64(val.Uint())
+	default:
+		err = fmt.Errorf("ToInt64 need numeric not `%T`", value)
+	}
+	return
+}
+
+// Int64ToBytes convert int64 to []byte
+func Int64ToBytes(i int64) []byte {
+	var buf = make([]byte, 8)
+	binary.BigEndian.PutUint64(buf, uint64(i))
+	return buf
+}
+
+// BytesToInt64 convert []byte to int64
+func BytesToInt64(buf []byte) int64 {
+	return int64(binary.BigEndian.Uint64(buf))
+}
