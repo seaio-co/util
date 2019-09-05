2e17b43011befeb9f5babd782d10b834d6c43e7e
Kevin123181 <Kevin123181@outlook.com>
2019-09-05 10:06:22 +0800

    Add corresponding write methods

---
+++ file/write.go
@@ -0,0 +1,18 @@
+package file
+
+import (
+	"os"
+	"path"
+)
+
+// WriteBytesToFile saves content type '[]byte' to file by given path.
+// It returns error when fail to finish operation.
+func WriteBytesToFile(filePath string, b []byte) (int, error) {
+	os.MkdirAll(path.Dir(filePath), os.ModePerm)
+	fw, err := os.Create(filePath)
+	if err != nil {
+		return 0, err
+	}
+	defer fw.Close()
+	return fw.Write(b)
+}
