5db4789a69f464b886cb8ebe757308795538d917
kerryalid <kerryalid@foxmaii.cn>
2019-09-10 09:22:36 +0800

    Add merge slice method in the common file.

--- conv/slice.go
+++ conv/slice.go
@@ -10,7 +10,7 @@ func SliceContains(sl []interface{}, v interface{}) bool {
 	return false
 }
 
-// SliceContainsInt 判断Int类型s1中是否包含v
+// SliceContainsInt 判断[]int类型s1中是否包含v
 func SliceContainsInt(sl []int, v int) bool {
 	for _, vv := range sl {
 		if vv == v {
@@ -20,6 +20,7 @@ func SliceContainsInt(sl []int, v int) bool {
 	return false
 }
 
+// SliceContainsInt64 判断[]int64类型s1中是否包含v
 func SliceContainsInt64(sl []int64, v int64) bool {
 	for _, vv := range sl {
 		if vv == v {
@@ -29,6 +30,7 @@ func SliceContainsInt64(sl []int64, v int64) bool {
 	return false
 }
 
+// SliceContainsString 判断[]string类型的s1中是否包含 v
 func SliceContainsString(sl []string, v string) bool {
 	for _, vv := range sl {
 		if vv == v {
@@ -36,4 +38,16 @@ func SliceContainsString(sl []string, v string) bool {
 		}
 	}
 	return false
+}
+
+// SliceMerge merges interface slices to one slice.
+func SliceMerge(slice1, slice2 []interface{}) (c []interface{}) {
+	c = append(slice1, slice2...)
+	return
+}
+
+// SliceMerge merges []int slices to one slice.
+func SliceMergeInt(slice1, slice2 []int) (c []int) {
+	c = append(slice1, slice2...)
+	return
 }
