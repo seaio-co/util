0dc694b631a9c2aecd0fa22e47ad3799c8a3dd17
august-star <august@hotmali.cn>
2019-09-11 09:25:58 +0800

    Complete the slice methods.

--- conv/slice.go
+++ conv/slice.go
@@ -51,3 +51,84 @@ func SliceMergeInt(slice1, slice2 []int) (c []int) {
 	c = append(slice1, slice2...)
 	return
 }
+
+// SliceMerge merges []int64 slices to one slice.
+func SliceMergeInt64(slice1, slice2 []int64) (c []int64) {
+	c = append(slice1, slice2...)
+	return
+}
+
+// SliceMerge merges []string slices to one slice.
+func SliceMergeString(slice1, slice2 []string) (c []string) {
+	c = append(slice1, slice2...)
+	return
+}
+
+func SliceUniqueInt64(s []int64) []int64 {
+	size := len(s)
+	if size == 0 {
+		return []int64{}
+	}
+
+	m := make(map[int64]bool)
+	for i := 0; i < size; i++ {
+		m[s[i]] = true
+	}
+
+	realLen := len(m)
+	ret := make([]int64, realLen)
+
+	idx := 0
+	for key := range m {
+		ret[idx] = key
+		idx++
+	}
+
+	return ret
+}
+
+func SliceUniqueInt(s []int) []int {
+	size := len(s)
+	if size == 0 {
+		return []int{}
+	}
+
+	m := make(map[int]bool)
+	for i := 0; i < size; i++ {
+		m[s[i]] = true
+	}
+
+	realLen := len(m)
+	ret := make([]int, realLen)
+
+	idx := 0
+	for key := range m {
+		ret[idx] = key
+		idx++
+	}
+
+	return ret
+}
+
+func SliceUniqueString(s []string) []string {
+	size := len(s)
+	if size == 0 {
+		return []string{}
+	}
+
+	m := make(map[string]bool)
+	for i := 0; i < size; i++ {
+		m[s[i]] = true
+	}
+
+	realLen := len(m)
+	ret := make([]string, realLen)
+
+	idx := 0
+	for key := range m {
+		ret[idx] = key
+		idx++
+	}
+
+	return ret
+}
