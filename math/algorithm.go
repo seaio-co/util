a505c5cba6a53058e80289391b85e5b74d7d5b0b
kerryalid <kerryalid@foxmaii.cn>
2019-09-27 16:10:45 +0800

    accumulate algorithm library

--- math/algorithm.go
+++ math/algorithm.go
@@ -1,1 +1,22 @@
 package math
+
+// BubbleSort
+func BubbleSort(buf []int) []int {
+	times := 0
+	for i := 0; i < len(buf)-1; i++ {
+		flag := false
+		for j := 1; j < len(buf)-i; j++ {
+			if buf[j-1] > buf[j] {
+				times++
+				tmp := buf[j-1]
+				buf[j-1] = buf[j]
+				buf[j] = tmp
+				flag = true
+			}
+		}
+		if !flag {
+			break
+		}
+	}
+	return buf
+}
