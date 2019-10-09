054df349741fa3234b5445010bdb0a2c80237d5a
Kevin123181 <Kevin123181@outlook.com>
2019-10-09 16:29:58 +0800

    Collate public method

--- common/uuid.go
+++ common/uuid.go
@@ -2,54 +2,12 @@ package common
 
 import (
 	"crypto/rand"
-	"encoding/hex"
 	"fmt"
 	"io"
-	"regexp"
-	"strings"
 )
 
 // UUID
 type UUID [16]byte
-
-var (
-	block1 = "[0-9a-f]{8}"
-	block2 = "[0-9a-f]{4}"
-	block3 = "[0-9a-f]{4}"
-	block4 = "[0-9a-f]{4}"
-	block5 = "[0-9a-f]{12}"
-
-	UUIDSnippet = block1 + "-" + block2 + "-" + block3 + "-" + block4 + "-" + block5
-	validUUID   = regexp.MustCompile("^" + UUIDSnippet + "$")
-)
-
-func UUIDFromString(s string) (UUID, error) {
-	if !IsValidUUIDString(s) {
-		return UUID{}, fmt.Errorf("invalid UUID: %q", s)
-	}
-	s = strings.Replace(s, "-", "", 4)
-	raw, err := hex.DecodeString(s)
-	if err != nil {
-		return UUID{}, err
-	}
-	var uuid UUID
-	copy(uuid[:], raw)
-	return uuid, nil
-}
-
-// IsValidUUIDString returns true, if the given string matches a valid UUID (version 4, variant 2).
-func IsValidUUIDString(s string) bool {
-	return validUUID.MatchString(s)
-}
-
-// MustNewUUID returns a new uuid, if an error occurs it panics.
-func MustNewUUID() UUID {
-	uuid, err := NewUUID()
-	if err != nil {
-		panic(err)
-	}
-	return uuid
-}
 
 // NewUUID generates a new version 4 UUID relying only on random numbers.
 func NewUUID() (UUID, error) {
