ee5b6af29feea1f013b96ddef241958a8056185c
kerryalid <kerryalid@foxmaii.cn>
2019-10-09 16:25:03 +0800

    add shell sort

--- math/algorithm.go
+++ math/algorithm.go
@@ -81,3 +81,26 @@ func InsertSort(buf []int) []int {
 	}
 	return buf
 }
+
+// ShellSort
+func ShellSort(arr []int) []int {
+	increment:=len(arr)
+	for {
+		increment = increment / 2
+		for i := 0; i < increment; i++ {
+			for j := i + increment; j < len(arr); j = j + increment {
+				for k := j; k > i; k = k - increment {
+					if arr[k] < arr[k-increment] {
+						arr[k], arr[k-increment] = arr[k-increment], arr[k]
+					} else {
+						break
+					}
+				}
+			}
+		}
+		if increment == 1 {
+			break
+		}
+	}
+	return arr
+}
