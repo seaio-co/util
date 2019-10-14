15334435ea34d21afd3412c7ea8c73381e9950ab
Abby-yc <Abby11991@outlook.com>
2019-10-14 13:58:36 +0800

    update proof of work file

--- block/proof of work.go
+++ block/proof of work.go
@@ -20,9 +20,7 @@ type ProofOfWork struct {
 func NewProofOfWork(b *Block) *ProofOfWork {
 	target := big.NewInt(1)
 	target.Lsh(target, uint(256-targetBits))
-
 	pow := &ProofOfWork{b, target}
-
 	return pow
 }
 
