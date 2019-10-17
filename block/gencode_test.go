24f095b7cc7d9bf1a58c7329f3b4fc4492e72c02
Abby-yc <Abby11991@outlook.com>
2019-10-17 18:47:08 +0800

    add gencode file

--- block/gencode_test.go
+++ block/gencode_test.go
@@ -1,1 +1,116 @@
-package main
+package bench
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGencodeSize(t *testing.T) {
+	p := Group{
+		Name: "test",
+		Members: []Person{
+			{
+				Name:   "John",
+				Age:    21,
+				Height: 5.9,
+			},
+			{
+				Name:   "Tom",
+				Age:    23,
+				Height: 5.8,
+			},
+			{
+				Name:   "Alan",
+				Age:    24,
+				Height: 6,
+			},
+		},
+	}
+	buf, _ := p.Marshal(nil)
+	fmt.Printf("Gencode encoded size: %v\n", len(buf))
+}
+
+func BenchmarkGencodeSerialize(b *testing.B) {
+	p := Group{
+		Name: "test",
+		Members: []Person{
+			{
+				Name:   "John",
+				Age:    21,
+				Height: 5.9,
+			},
+			{
+				Name:   "Tom",
+				Age:    23,
+				Height: 5.8,
+			},
+			{
+				Name:   "Alan",
+				Age:    24,
+				Height: 6,
+			},
+		},
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		p.Marshal(nil)
+	}
+}
+
+func BenchmarkGencodeDeserialize(b *testing.B) {
+	p := Group{
+		Name: "test",
+		Members: []Person{
+			{
+				Name:   "John",
+				Age:    21,
+				Height: 5.9,
+			},
+			{
+				Name:   "Tom",
+				Age:    23,
+				Height: 5.8,
+			},
+			{
+				Name:   "Alan",
+				Age:    24,
+				Height: 6,
+			},
+		},
+	}
+	buf, _ := p.Marshal(nil)
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		p.Unmarshal(buf)
+	}
+}
+
+func BenchmarkFixedGencodeSerialize(b *testing.B) {
+	p := Fixed{
+		A: -5,
+		B: 6,
+		C: 6.7,
+		D: 12.65,
+	}
+	buf, _ := p.Marshal(nil)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		p.Marshal(buf)
+	}
+}
+
+func BenchmarkFixedGencodeDeserialize(b *testing.B) {
+	p := Fixed{
+		A: -5,
+		B: 6,
+		C: 6.7,
+		D: 12.65,
+	}
+	buf, _ := p.Marshal(nil)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		p.Unmarshal(buf)
+	}
+}
