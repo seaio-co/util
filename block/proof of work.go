64b13140f35b20f36be3eba26da49dfb3818ad56
louis11911 <louis11911@outlook.com>
2019-10-31 20:57:09 +0800

    modified proof file

--- block/proof of work.go
+++ block/proof of work.go
@@ -42,3 +42,40 @@ func (pow *ProofOfWork) prepareData(nonce int) []byte {
 
 	return data
 }
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+
+	fmt.Printf("Mining a new block")
+	for nonce < maxNonce {
+		data := pow.prepareData(nonce)
+
+		hash = sha256.Sum256(data)
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.target) == -1 {
+			fmt.Printf("\r%x", hash)
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Print("\n\n")
+
+	return nonce, hash[:]
+}
+
+// Validate block's PoW
+func (pow *ProofOfWork) Validate() bool {
+	var hashInt big.Int
+
+	data := pow.prepareData(pow.block.Nonce)
+	hash := sha256.Sum256(data)
+	hashInt.SetBytes(hash[:])
+
+	isValid := hashInt.Cmp(pow.target) == -1
+
+	return isValid
+}
