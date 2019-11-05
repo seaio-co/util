345a8f351acb0905ad6e26404c38972ccd4480bf
Kevin123181 <Kevin123181@outlook.com>
2019-11-05 20:33:38 +0800

    add go_web file

---
+++ block/go_web.go
@@ -0,0 +1,19 @@
+package main
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"log"
+)
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println(r.Form)
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello Wrold!")
+}
