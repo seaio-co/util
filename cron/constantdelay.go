fa70b0f988efeaad8bf805b6a8641d1fce7ecdc0
Kevin123181 <Kevin123181@outlook.com>
2019-12-04 20:37:39 +0800

    constantdelay

---
+++ cron/constantdelay.go
@@ -0,0 +1,9 @@
+package cron
+
+import "time"
+
+// ConstantDelaySchedule represents a simple recurring duty cycle, e.g. "Every 5 minutes".
+// It does not support jobs more frequent than once a second.
+type ConstantDelaySchedule struct {
+	Delay time.Duration
+}
