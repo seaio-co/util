770e8ddb299e77cb44b5e768732e89dd40a1c811
august-star <august@hotmali.cn>
2019-12-06 18:07:27 +0800

    returns the next time this should be run

--- cron/constantdelay.go
+++ cron/constantdelay.go
@@ -19,3 +19,9 @@ func Every(duration time.Duration) ConstantDelaySchedule {
 		Delay: duration - time.Duration(duration.Nanoseconds())%time.Second,
 	}
 }
+
+// Next returns the next time this should be run.
+// This rounds so that the next activation time will be on the second.
+func (schedule ConstantDelaySchedule) Next(t time.Time) time.Time {
+	return t.Add(schedule.Delay - time.Duration(t.Nanosecond())*time.Nanosecond)
+}
