dfaba3817056a03f334fcec93ab92131157b48b6
Kevin123181 <Kevin123181@outlook.com>
2020-01-16 14:29:44 +0800

    imgcat/imgcat.go

---
+++ imgcat/imgcat.go
@@ -0,0 +1,205 @@
+package imgcat
+
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+// An Option modifies how an image is displayed.
+type Option string
+
+// Length is used by the Width and Height options.
+type Length string
+
+// Cells gives a length in character cells.
+func Cells(x int) Length { return Length(fmt.Sprint(x)) }
+
+// Pixels gives a length in pixels.
+func Pixels(x int) Length { return Length(fmt.Sprintf("%dpx", x)) }
+
+// Percent gives relative length to the session's width or height.
+func Percent(x int) Length { return Length(fmt.Sprintf("%d%%", x)) }
+
+// Auto keeps the image to its inherent size.
+func Auto() Length { return Length("auto") }
+
+// Name sents the filename for the image. Defaults to "Unnamed file".
+func Name(name string) Option {
+	buf := new(bytes.Buffer)
+	enc := base64.NewEncoder(base64.StdEncoding, buf)
+	fmt.Fprint(enc, name)
+	if err := enc.Close(); err != nil {
+		log.Fatalf("could not encode to buffer: %v", err)
+	}
+	return Option(fmt.Sprintf("name=%s", buf))
+}
+
+// Size sets the file size in bytes. It's only used by the progress indicator.
+func Size(size int) Option {
+	return Option(fmt.Sprintf("size=%d", size))
+}
+
+// Width to render, it can be in cells, pixels, percentage, or auto.
+func Width(l Length) Option {
+	return Option(fmt.Sprintf("width=%s", l))
+}
+
+// Height to render, it can be in cells, pixels, percentage, or auto.
+func Height(l Length) Option {
+	return Option(fmt.Sprintf("height=%s", l))
+}
+
+func boolToInt(b bool) int {
+	if b {
+		return 1
+	}
+	return 0
+}
+
+// PreserveAspectRatio set to false causes the image's inherent
+// aspect ratio will not be respected; otherwise, it will fill the
+// specified width and height as much as possible without stretching.
+// Defaults to true.
+func PreserveAspectRatio(b bool) Option {
+	return Option(fmt.Sprintf("preserveAspectRatio=%d", boolToInt(b)))
+}
+
+// Inline set to true causes the to be displayed inline.
+// Otherwise, it will be downloaded with no visual
+// representation in the terminal session.
+// Defaults to false.
+func Inline(b bool) Option {
+	return Option(fmt.Sprintf("inline=%d", boolToInt(b)))
+}
+
+// IsSupported check whether imgcat works in the current terminal.
+func IsSupported() bool { return isSupported() }
+
+// Can be swapped for testing.
+var isSupported = func() bool {
+	return os.Getenv("TERM_PROGRAM") == "iTerm.app"
+}
+
+// IsTmux checks whether we are in a tmux window.
+// tmux requires different escape code than iterm2 alone.
+// NOTE: If not using the iterm2 tmux integration (tmux -CC),
+// the prompt and cursor of tmux will not be aware of
+// the iterm2 sizing of the image and will be placed
+// over the image.
+func IsTmux() bool { return isTmux() }
+
+var isTmux = func() bool {
+	// for testing, set TMUX_TEST true/false
+	if os.Getenv("TMUX_TEST") == "false" {
+		return false
+	}
+	if os.Getenv("TMUX_TEST") == "true" {
+		return true
+	}
+	// otherwise, determine from TERM, TMUX variables
+	return (os.Getenv("TERM") == "screen" || len(os.Getenv("TMUX")) > 0)
+}
+
+//set the header string depending on whether we are in tmux.
+func headerEscape() string {
+	if IsTmux() {
+		return "\x1bPtmux;\x1b\x1b]1337;File="
+	}
+	return "\x1b]1337;File="
+}
+
+//set the header string depending on whether we are in tmux.
+func footerEscape() string {
+	if IsTmux() {
+		return "\a\x1b\\\n"
+	}
+	return "\a\n"
+}
+
+// NewEncoder returns a encoder that encodes images for iterm2.
+func NewEncoder(w io.Writer, options ...Option) (*Encoder, error) {
+	if !IsSupported() {
+		return nil, fmt.Errorf("imgcat is only supported with iTerm2")
+	}
+
+	enc := &Encoder{out: w, options: options}
+
+	return enc, nil
+}
+
+// An Encoder is used to encode images to iterm2.
+type Encoder struct {
+	out     io.Writer
+	options []Option
+}
+
+// Encode encodes the given image into the output.
+func (enc *Encoder) Encode(r io.Reader) error {
+	header := new(bytes.Buffer)
+	fmt.Fprint(header, headerEscape())
+	for i, option := range enc.options {
+		fmt.Fprintf(header, "%s", option)
+		if i < len(enc.options)-1 {
+			fmt.Fprintf(header, ";")
+		}
+	}
+	fmt.Fprintf(header, ":")
+	pr, pw := io.Pipe()
+	go func() {
+		enc := base64.NewEncoder(base64.StdEncoding, pw)
+		defer func() {
+			if err := enc.Close(); err != nil {
+				// always returns nil according to specs.
+				_ = pw.CloseWithError(err)
+
+			}
+		}()
+
+		_, err := io.Copy(enc, r)
+		if err != nil {
+			// always returns nil according to specs.
+			_ = pw.CloseWithError(err)
+		} else {
+			// always returns nil according to specs.
+			_ = pw.CloseWithError(enc.Close())
+		}
+	}()
+
+	footer := bytes.NewBufferString(footerEscape())
+
+	_, err := io.Copy(enc.out, io.MultiReader(header, pr, footer))
+	return err
+}
+
+// Writer creates a writer that will encode whatever is written to it.
+func (enc *Encoder) Writer() io.WriteCloser {
+	pr, pw := io.Pipe()
+	w := &writer{pw, make(chan struct{})}
+	go func() {
+		defer close(w.done)
+		if err := enc.Encode(pr); err != nil {
+			// always returns nil according to specs.
+			_ = pr.CloseWithError(err)
+		}
+	}()
+	return w
+}
+
+type writer struct {
+	pw   *io.PipeWriter
+	done chan struct{}
+}
+
+func (w *writer) Write(p []byte) (int, error) { return w.pw.Write(p) }
+
+func (w *writer) Close() error {
+	if err := w.pw.Close(); err != nil {
+		return err
+	}
+	<-w.done
+	return nil
+}
---
+++ imgcat/imgcat_test.go
@@ -0,0 +1,169 @@
+package imgcat
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestIsSupported(t *testing.T) {
+	defer func(old string) { check(t, os.Setenv("TERM_PROGRAM", old)) }(os.Getenv("TERM_PROGRAM"))
+	check(t, os.Setenv("TERM_PROGRAM", "foo"))
+	if _, err := NewEncoder(nil); err == nil {
+		t.Fatal("imgcat should not be supported now")
+	}
+}
+
+func TestEncode(t *testing.T) {
+	// Change is supported to be always true and restore at the end.
+	defer func(old func() bool) { isSupported = old }(isSupported)
+	// Unset the TMUX_TEST variable after we set it.
+	defer func() { check(t, os.Unsetenv("TMUX_TEST")) }()
+	isSupported = func() bool { return true }
+	// shut off tmux detection for these tests
+	check(t, os.Setenv("TMUX_TEST", "false"))
+
+	tc := []struct {
+		name    string
+		in      string
+		options []Option
+		out     string
+	}{
+		// {"empty", "", nil, "\x1b]1337;File=:\a\n"},
+		{"test", "test", nil, "\x1b]1337;File=:dGVzdA==\a\n"},
+		{"test inline", "test", []Option{Inline(true)}, "\x1b]1337;File=inline=1:dGVzdA==\a\n"},
+		{"test outline", "test", []Option{Inline(false)}, "\x1b]1337;File=inline=0:dGVzdA==\a\n"},
+		{"test with name", "test", []Option{Name("test")}, "\x1b]1337;File=name=dGVzdA==:dGVzdA==\a\n"},
+		{"test width 10 cells", "test", []Option{Width(Cells(10))}, "\x1b]1337;File=width=10:dGVzdA==\a\n"},
+		{"test width 10px", "test", []Option{Width(Pixels(10))}, "\x1b]1337;File=width=10px:dGVzdA==\a\n"},
+		{"test width 10%", "test", []Option{Width(Percent(10))}, "\x1b]1337;File=width=10%:dGVzdA==\a\n"},
+		{"test width auto", "test", []Option{Width(Auto())}, "\x1b]1337;File=width=auto:dGVzdA==\a\n"},
+		{"test with size", "test", []Option{Size(42)}, "\x1b]1337;File=size=42:dGVzdA==\a\n"},
+		{"test height 10 cells", "test", []Option{Height(Cells(10))}, "\x1b]1337;File=height=10:dGVzdA==\a\n"},
+		{"test height 10px", "test", []Option{Height(Pixels(10))}, "\x1b]1337;File=height=10px:dGVzdA==\a\n"},
+		{"test height 10%", "test", []Option{Height(Percent(10))}, "\x1b]1337;File=height=10%:dGVzdA==\a\n"},
+		{"test preserve aspect ration", "test", []Option{PreserveAspectRatio(true)}, "\x1b]1337;File=preserveAspectRatio=1:dGVzdA==\a\n"},
+		{"test don't preserve aspect ration", "test", []Option{PreserveAspectRatio(false)}, "\x1b]1337;File=preserveAspectRatio=0:dGVzdA==\a\n"},
+		{"all options together", "test", []Option{
+			Inline(true), Name("test"), Width(Percent(10)), Height(Percent(10)), PreserveAspectRatio(false), Size(42),
+		}, "\x1b]1337;File=inline=1;name=dGVzdA==;width=10%;height=10%;preserveAspectRatio=0;size=42:dGVzdA==\a\n"},
+	}
+
+	for _, tt := range tc {
+		t.Run(tt.name, func(t *testing.T) {
+			var buf bytes.Buffer
+			enc, err := NewEncoder(&buf, tt.options...)
+			if err != nil {
+				t.Fatalf("could not create writer: %v", err)
+			}
+			if err := enc.Encode(strings.NewReader(tt.in)); err != nil {
+				t.Fatalf("could not write: %v", err)
+			}
+			if got := buf.String(); got != tt.out {
+				t.Fatalf("expected output %q; got %q", tt.out, got)
+			}
+		})
+	}
+}
+
+type badWriter struct{}
+
+func (badWriter) Write(p []byte) (int, error) {
+	return 0, fmt.Errorf("bad writer")
+}
+
+func TestEncoder(t *testing.T) {
+	// Change is supported to be always true and restore at the end.
+	defer func(old func() bool) { isSupported = old }(isSupported)
+	isSupported = func() bool { return true }
+
+	enc, err := NewEncoder(badWriter{})
+	if err != nil {
+		t.Fatalf("could not create writer: %v", err)
+	}
+	err = enc.Encode(strings.NewReader("test"))
+	if err == nil {
+		t.Fatalf("expected error; got nothing")
+	}
+	if err.Error() != "bad writer" {
+		t.Fatalf("expected error bad writer; got %v", err)
+	}
+}
+
+func TestGoodWriter(t *testing.T) {
+	// Change is supported to be always true and restore at the end.
+	defer func(old func() bool) { isSupported = old }(isSupported)
+	defer func() { check(t, os.Unsetenv("TMUX_TEST")) }()
+	isSupported = func() bool { return true }
+	// shut off tmux detection for these tests
+	check(t, os.Setenv("TMUX_TEST", "false"))
+
+	tc := []struct {
+		name    string
+		in      string
+		options []Option
+		out     string
+	}{
+		// {"empty", "", nil, "\x1b]1337;File=:\a\n"},
+		{"test", "test", nil, "\x1b]1337;File=:dGVzdA==\a\n"},
+		{"test inline", "test", []Option{Inline(true)}, "\x1b]1337;File=inline=1:dGVzdA==\a\n"},
+		{"test outline", "test", []Option{Inline(false)}, "\x1b]1337;File=inline=0:dGVzdA==\a\n"},
+		{"test with name", "test", []Option{Name("test")}, "\x1b]1337;File=name=dGVzdA==:dGVzdA==\a\n"},
+		{"test width 10 cells", "test", []Option{Width(Cells(10))}, "\x1b]1337;File=width=10:dGVzdA==\a\n"},
+		{"test width 10px", "test", []Option{Width(Pixels(10))}, "\x1b]1337;File=width=10px:dGVzdA==\a\n"},
+		{"test width 10%", "test", []Option{Width(Percent(10))}, "\x1b]1337;File=width=10%:dGVzdA==\a\n"},
+		{"test width auto", "test", []Option{Width(Auto())}, "\x1b]1337;File=width=auto:dGVzdA==\a\n"},
+		{"test with size", "test", []Option{Size(42)}, "\x1b]1337;File=size=42:dGVzdA==\a\n"},
+		{"test height 10 cells", "test", []Option{Height(Cells(10))}, "\x1b]1337;File=height=10:dGVzdA==\a\n"},
+		{"test height 10px", "test", []Option{Height(Pixels(10))}, "\x1b]1337;File=height=10px:dGVzdA==\a\n"},
+		{"test height 10%", "test", []Option{Height(Percent(10))}, "\x1b]1337;File=height=10%:dGVzdA==\a\n"},
+		{"test preserve aspect ration", "test", []Option{PreserveAspectRatio(true)}, "\x1b]1337;File=preserveAspectRatio=1:dGVzdA==\a\n"},
+		{"test don't preserve aspect ration", "test", []Option{PreserveAspectRatio(false)}, "\x1b]1337;File=preserveAspectRatio=0:dGVzdA==\a\n"},
+		{"all options together", "test", []Option{
+			Inline(true), Name("test"), Width(Percent(10)), Height(Percent(10)), PreserveAspectRatio(false), Size(42),
+		}, "\x1b]1337;File=inline=1;name=dGVzdA==;width=10%;height=10%;preserveAspectRatio=0;size=42:dGVzdA==\a\n"},
+	}
+
+	for _, tt := range tc {
+		t.Run(tt.name, func(t *testing.T) {
+			var buf bytes.Buffer
+			enc, err := NewEncoder(&buf, tt.options...)
+			if err != nil {
+				t.Fatalf("could not create writer: %v", err)
+			}
+			wc := enc.Writer()
+			if _, err := fmt.Fprint(wc, tt.in); err != nil {
+				t.Fatalf("could not write: %v", err)
+			}
+			if err := wc.Close(); err != nil {
+				t.Fatalf("could not close: %v", err)
+			}
+			if got := buf.String(); got != tt.out {
+				t.Fatalf("expected output %q; got %q", tt.out, got)
+			}
+		})
+	}
+}
+
+func TestBadWriter(t *testing.T) {
+	// Change is supported to be always true and restore at the end.
+	defer func(old func() bool) { isSupported = old }(isSupported)
+	isSupported = func() bool { return true }
+
+	enc, err := NewEncoder(badWriter{})
+	if err != nil {
+		t.Fatalf("could not create writer: %v", err)
+	}
+	wc := enc.Writer()
+	if _, err := fmt.Fprint(wc, "hello"); err == nil || err.Error() != "bad writer" {
+		t.Fatalf("expected error bad writer; got %v", err)
+	}
+}
+
+func check(t *testing.T, err error) {
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+}
