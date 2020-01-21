c46eac0ae26d9a5f9d6e929071d7a58b7fb68f18
Kevin123181 <Kevin123181@outlook.com>
2020-01-21 15:30:22 +0800

    pidfile

---
+++ pidfile/pidfile.go
@@ -0,0 +1,64 @@
+package pidfile
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// PIDFile is a file used to store the process ID of a running process.
+type PIDFile struct {
+	path string
+}
+
+func checkPIDFileAlreadyExists(path string) error {
+	if pidByte, err := ioutil.ReadFile(path); err == nil {
+		pidString := strings.TrimSpace(string(pidByte))
+		if pid, err := strconv.Atoi(pidString); err == nil {
+			if processExists(pid) {
+				return fmt.Errorf("pid [%s] file found: %s", string(pidByte), path)
+			}
+		}
+
+	}
+	return nil
+}
+
+// New creates a PIDfile using the specified path.
+func New(path string) (*PIDFile, error) {
+	if err := checkPIDFileAlreadyExists(path); err != nil {
+		return nil, err
+	}
+	if err := ioutil.WriteFile(path, []byte(fmt.Sprintf("%d", os.Getpid())), 0644); err != nil {
+		return nil, err
+	}
+
+	return &PIDFile{path: path}, nil
+}
+
+// Remove removes the PIDFile.
+func (file PIDFile) Remove() error {
+	if err := os.Remove(file.path); err != nil {
+		return err
+	}
+	return nil
+}
+
+// ReadPIDFile 读取pid文件
+func ReadPIDFile(pidfile string) (pid int, err error) {
+	f, err := os.Open(pidfile)
+	if err != nil {
+		return
+	}
+
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		return
+	}
+
+	pid64, err := strconv.ParseInt(string(b), 10, 64)
+	pid = int(pid64)
+	return
+}
---
+++ pidfile/pidfile_unix.go
@@ -0,0 +1,14 @@
+package pidfile
+
+import (
+	"os"
+	"path/filepath"
+	"strconv"
+)
+
+func processExists(pid int) bool {
+	if _, err := os.Stat(filepath.Join("/proc", strconv.Itoa(pid))); err == nil {
+		return true
+	}
+	return false
+}
