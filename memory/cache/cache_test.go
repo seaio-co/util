78d6f26380a0d44cd39488230b14f5dc401e9c6a
clement-collab <clement@hotmali.cn>
2020-02-29 12:55:02 +0800

    TestCacheSet

--- memory/cache/cache_test.go
+++ memory/cache/cache_test.go
@@ -3,6 +3,7 @@ package cache
 import (
 	"math/rand"
 	"strings"
+	"sync"
 	"testing"
 	"time"
 
@@ -139,4 +140,191 @@ func TestMultipleClose(t *testing.T) {
 	require.NoError(t, err)
 	c.Close()
 	c.Close()
+}
+
+func TestCacheProcessItems(t *testing.T) {
+	m := &sync.Mutex{}
+	evicted := make(map[uint64]struct{})
+	c, err := NewCache(&Config{
+		NumCounters: 100,
+		MaxCost:     10,
+		BufferItems: 64,
+		Cost: func(value interface{}) int64 {
+			return int64(value.(int))
+		},
+		OnEvict: func(key, conflict uint64, value interface{}, cost int64) {
+			m.Lock()
+			defer m.Unlock()
+			evicted[key] = struct{}{}
+		},
+	})
+	require.NoError(t, err)
+
+	var key uint64
+	var conflict uint64
+
+	key, conflict = z.KeyToHash(1)
+	c.setBuf <- &item{
+		flag:     itemNew,
+		key:      key,
+		conflict: conflict,
+		value:    1,
+		cost:     0,
+	}
+	time.Sleep(wait)
+	require.True(t, c.policy.Has(1))
+	require.Equal(t, int64(1), c.policy.Cost(1))
+
+	key, conflict = z.KeyToHash(1)
+	c.setBuf <- &item{
+		flag:     itemUpdate,
+		key:      key,
+		conflict: conflict,
+		value:    2,
+		cost:     0,
+	}
+	time.Sleep(wait)
+	require.Equal(t, int64(2), c.policy.Cost(1))
+
+	key, conflict = z.KeyToHash(1)
+	c.setBuf <- &item{
+		flag:     itemDelete,
+		key:      key,
+		conflict: conflict,
+	}
+	time.Sleep(wait)
+	key, conflict = z.KeyToHash(1)
+	val, ok := c.store.Get(key, conflict)
+	require.False(t, ok)
+	require.Nil(t, val)
+	require.False(t, c.policy.Has(1))
+
+	key, conflict = z.KeyToHash(2)
+	c.setBuf <- &item{
+		flag:     itemNew,
+		key:      key,
+		conflict: conflict,
+		value:    2,
+		cost:     3,
+	}
+	key, conflict = z.KeyToHash(3)
+	c.setBuf <- &item{
+		flag:     itemNew,
+		key:      key,
+		conflict: conflict,
+		value:    3,
+		cost:     3,
+	}
+	key, conflict = z.KeyToHash(4)
+	c.setBuf <- &item{
+		flag:     itemNew,
+		key:      key,
+		conflict: conflict,
+		value:    3,
+		cost:     3,
+	}
+	key, conflict = z.KeyToHash(5)
+	c.setBuf <- &item{
+		flag:     itemNew,
+		key:      key,
+		conflict: conflict,
+		value:    3,
+		cost:     5,
+	}
+	time.Sleep(wait)
+	m.Lock()
+	require.NotEqual(t, 0, len(evicted))
+	m.Unlock()
+
+	defer func() {
+		require.NotNil(t, recover())
+	}()
+	c.Close()
+	c.setBuf <- &item{flag: itemNew}
+}
+
+func TestCacheGet(t *testing.T) {
+	c, err := NewCache(&Config{
+		NumCounters: 100,
+		MaxCost:     10,
+		BufferItems: 64,
+		Metrics:     true,
+	})
+	require.NoError(t, err)
+
+	key, conflict := z.KeyToHash(1)
+	i := item{
+		key:      key,
+		conflict: conflict,
+		value:    1,
+	}
+	c.store.Set(&i)
+	val, ok := c.Get(1)
+	require.True(t, ok)
+	require.NotNil(t, val)
+
+	val, ok = c.Get(2)
+	require.False(t, ok)
+	require.Nil(t, val)
+
+	// 0.5 and not 1.0 because we tried Getting each item twice
+	require.Equal(t, 0.5, c.Metrics.Ratio())
+
+	c = nil
+	val, ok = c.Get(0)
+	require.False(t, ok)
+	require.Nil(t, val)
+}
+
+// retrySet calls SetWithTTL until the item is accepted by the cache.
+func retrySet(t *testing.T, c *Cache, key, value int, cost int64, ttl time.Duration) {
+	for {
+		if set := c.SetWithTTL(key, value, cost, ttl); !set {
+			time.Sleep(wait)
+			continue
+		}
+
+		time.Sleep(wait)
+		val, ok := c.Get(key)
+		require.True(t, ok)
+		require.NotNil(t, val)
+		require.Equal(t, value, val.(int))
+		return
+	}
+}
+
+func TestCacheSet(t *testing.T) {
+	c, err := NewCache(&Config{
+		NumCounters: 100,
+		MaxCost:     10,
+		BufferItems: 64,
+		Metrics:     true,
+	})
+	require.NoError(t, err)
+
+	retrySet(t, c, 1, 1, 1, 0)
+
+	c.Set(1, 2, 2)
+	val, ok := c.store.Get(z.KeyToHash(1))
+	require.True(t, ok)
+	require.Equal(t, 2, val.(int))
+
+	c.stop <- struct{}{}
+	for i := 0; i < setBufSize; i++ {
+		key, conflict := z.KeyToHash(1)
+		c.setBuf <- &item{
+			flag:     itemUpdate,
+			key:      key,
+			conflict: conflict,
+			value:    1,
+			cost:     1,
+		}
+	}
+	require.False(t, c.Set(2, 2, 1))
+	require.Equal(t, uint64(1), c.Metrics.SetsDropped())
+	close(c.setBuf)
+	close(c.stop)
+
+	c = nil
+	require.False(t, c.Set(1, 1, 1))
 }
