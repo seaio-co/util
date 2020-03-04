52ea9413d0629017c9afb8c1d527d25ed2c0b3c7
Kevin123181 <Kevin123181@outlook.com>
2020-03-04 10:55:14 +0800

    sampledLFU tinyLFU

--- memory/cache/policy.go
+++ memory/cache/policy.go
@@ -3,6 +3,8 @@ package cache
 import (
 	"math"
 	"sync"
+
+	"github.com/dgraph-io/ristretto/z"
 )
 
 const (
@@ -227,4 +229,135 @@ func (p *defaultPolicy) Close() {
 	p.stop <- struct{}{}
 	close(p.stop)
 	close(p.itemsCh)
+}
+
+// sampledLFU is an eviction helper storing key-cost pairs.
+type sampledLFU struct {
+	keyCosts map[uint64]int64
+	maxCost  int64
+	used     int64
+	metrics  *Metrics
+}
+
+func newSampledLFU(maxCost int64) *sampledLFU {
+	return &sampledLFU{
+		keyCosts: make(map[uint64]int64),
+		maxCost:  maxCost,
+	}
+}
+
+func (p *sampledLFU) roomLeft(cost int64) int64 {
+	return p.maxCost - (p.used + cost)
+}
+
+func (p *sampledLFU) fillSample(in []*policyPair) []*policyPair {
+	if len(in) >= lfuSample {
+		return in
+	}
+	for key, cost := range p.keyCosts {
+		in = append(in, &policyPair{key, cost})
+		if len(in) >= lfuSample {
+			return in
+		}
+	}
+	return in
+}
+
+func (p *sampledLFU) del(key uint64) {
+	cost, ok := p.keyCosts[key]
+	if !ok {
+		return
+	}
+	p.used -= cost
+	delete(p.keyCosts, key)
+	p.metrics.add(costEvict, key, uint64(cost))
+	p.metrics.add(keyEvict, key, 1)
+}
+
+func (p *sampledLFU) add(key uint64, cost int64) {
+	p.keyCosts[key] = cost
+	p.used += cost
+}
+
+func (p *sampledLFU) updateIfHas(key uint64, cost int64) bool {
+	if prev, found := p.keyCosts[key]; found {
+		// Update the cost of an existing key, but don't worry about evicting.
+		// Evictions will be handled the next time a new item is added.
+		p.metrics.add(keyUpdate, key, 1)
+		if prev > cost {
+			diff := prev - cost
+			p.metrics.add(costAdd, key, ^uint64(uint64(diff)-1))
+		} else if cost > prev {
+			diff := cost - prev
+			p.metrics.add(costAdd, key, uint64(diff))
+		}
+		p.used += cost - prev
+		p.keyCosts[key] = cost
+		return true
+	}
+	return false
+}
+
+func (p *sampledLFU) clear() {
+	p.used = 0
+	p.keyCosts = make(map[uint64]int64)
+}
+
+// tinyLFU is an admission helper that keeps track of access frequency using
+// tiny (4-bit) counters in the form of a count-min sketch.
+// tinyLFU is NOT thread safe.
+type tinyLFU struct {
+	freq    *cmSketch
+	door    *z.Bloom
+	incrs   int64
+	resetAt int64
+}
+
+func newTinyLFU(numCounters int64) *tinyLFU {
+	return &tinyLFU{
+		freq:    newCmSketch(numCounters),
+		door:    z.NewBloomFilter(float64(numCounters), 0.01),
+		resetAt: numCounters,
+	}
+}
+
+func (p *tinyLFU) Push(keys []uint64) {
+	for _, key := range keys {
+		p.Increment(key)
+	}
+}
+
+func (p *tinyLFU) Estimate(key uint64) int64 {
+	hits := p.freq.Estimate(key)
+	if p.door.Has(key) {
+		hits++
+	}
+	return hits
+}
+
+func (p *tinyLFU) Increment(key uint64) {
+	// Flip doorkeeper bit if not already done.
+	if added := p.door.AddIfNotHas(key); !added {
+		// Increment count-min counter if doorkeeper bit is already set.
+		p.freq.Increment(key)
+	}
+	p.incrs++
+	if p.incrs >= p.resetAt {
+		p.reset()
+	}
+}
+
+func (p *tinyLFU) reset() {
+	// Zero out incrs.
+	p.incrs = 0
+	// clears doorkeeper bits
+	p.door.Clear()
+	// halves count-min counters
+	p.freq.Reset()
+}
+
+func (p *tinyLFU) clear() {
+	p.incrs = 0
+	p.door.Clear()
+	p.freq.Clear()
 }
