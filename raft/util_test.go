c7e1e7a41695f2ee132a23db6fdfbf759d83711e
Kevin123181 <Kevin123181@outlook.com>
2020-03-27 09:19:29 +0800

    util_test

---
+++ raft/util_test.go
@@ -0,0 +1,63 @@
+package raft
+
+import (
+	"testing"
+	"time"
+)
+
+func TestRandomTimeout(t *testing.T) {
+	start := time.Now()
+	timeout := randomTimeout(time.Millisecond)
+
+	select {
+	case <-timeout:
+		diff := time.Now().Sub(start)
+		if diff < time.Millisecond {
+			t.Fatalf("fired early")
+		}
+	case <-time.After(3 * time.Millisecond):
+		t.Fatalf("timeout")
+	}
+}
+
+func TestNewSeed(t *testing.T) {
+	vals := make(map[int64]bool)
+	for i := 0; i < 1000; i++ {
+		seed := newSeed()
+		if _, exists := vals[seed]; exists {
+			t.Fatal("newSeed() return a value it'd previously returned")
+		}
+		vals[seed] = true
+	}
+}
+
+func TestRandomTimeout_NoTime(t *testing.T) {
+	timeout := randomTimeout(0)
+	if timeout != nil {
+		t.Fatalf("expected nil channel")
+	}
+}
+
+func TestMin(t *testing.T) {
+	if min(1, 1) != 1 {
+		t.Fatalf("bad min")
+	}
+	if min(2, 1) != 1 {
+		t.Fatalf("bad min")
+	}
+	if min(1, 2) != 1 {
+		t.Fatalf("bad min")
+	}
+}
+
+func TestMax(t *testing.T) {
+	if max(1, 1) != 1 {
+		t.Fatalf("bad max")
+	}
+	if max(2, 1) != 2 {
+		t.Fatalf("bad max")
+	}
+	if max(1, 2) != 2 {
+		t.Fatalf("bad max")
+	}
+}
