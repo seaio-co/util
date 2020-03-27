e204da8f5ff5d0d71e4f3ee5790b987e7b8d8381
kerryalid <kerryalid@foxmaii.cn>
2020-03-27 09:21:49 +0800

    util_test

--- raft/util_test.go
+++ raft/util_test.go
@@ -1,6 +1,7 @@
 package raft
 
 import (
+	"regexp"
 	"testing"
 	"time"
 )
@@ -59,5 +60,88 @@ func TestMax(t *testing.T) {
 	}
 	if max(1, 2) != 2 {
 		t.Fatalf("bad max")
+	}
+}
+
+func TestGenerateUUID(t *testing.T) {
+	prev := generateUUID()
+	for i := 0; i < 100; i++ {
+		id := generateUUID()
+		if prev == id {
+			t.Fatalf("Should get a new ID!")
+		}
+
+		matched, err := regexp.MatchString(
+			`[\da-f]{8}-[\da-f]{4}-[\da-f]{4}-[\da-f]{4}-[\da-f]{12}`, id)
+		if !matched || err != nil {
+			t.Fatalf("expected match %s %v %s", id, matched, err)
+		}
+	}
+}
+
+func TestBackoff(t *testing.T) {
+	b := backoff(10*time.Millisecond, 1, 8)
+	if b != 10*time.Millisecond {
+		t.Fatalf("bad: %v", b)
+	}
+
+	b = backoff(20*time.Millisecond, 2, 8)
+	if b != 20*time.Millisecond {
+		t.Fatalf("bad: %v", b)
+	}
+
+	b = backoff(10*time.Millisecond, 8, 8)
+	if b != 640*time.Millisecond {
+		t.Fatalf("bad: %v", b)
+	}
+
+	b = backoff(10*time.Millisecond, 9, 8)
+	if b != 640*time.Millisecond {
+		t.Fatalf("bad: %v", b)
+	}
+}
+
+func TestOverrideNotifyBool(t *testing.T) {
+	ch := make(chan bool, 1)
+
+	// sanity check - buffered channel don't have any values
+	select {
+	case v := <-ch:
+		t.Fatalf("unexpected receive: %v", v)
+	default:
+	}
+
+	// simple case of a single push
+	overrideNotifyBool(ch, false)
+	select {
+	case v := <-ch:
+		if v != false {
+			t.Fatalf("expected false but got %v", v)
+		}
+	default:
+		t.Fatalf("expected a value but is not ready")
+	}
+
+	// assert that function never blocks and only last item is received
+	overrideNotifyBool(ch, false)
+	overrideNotifyBool(ch, false)
+	overrideNotifyBool(ch, false)
+	overrideNotifyBool(ch, false)
+	overrideNotifyBool(ch, true)
+
+	select {
+	case v := <-ch:
+		if v != true {
+			t.Fatalf("expected true but got %v", v)
+		}
+	default:
+		t.Fatalf("expected a value but is not ready")
+	}
+
+	// no further value is available
+	select {
+	case v := <-ch:
+		t.Fatalf("unexpected receive: %v", v)
+	default:
 	}
 }
