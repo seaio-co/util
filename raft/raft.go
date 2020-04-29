999e0b9d3c5327507b7081b89b067b2980ad8791
louis11911 <louis11911@outlook.com>
2020-04-29 14:00:58 +0800

    Raft.getRPCHeader

---
+++ raft/raft.go
@@ -0,0 +1,24 @@
+package raft
+
+import (
+	"time"
+)
+
+const (
+	minCheckInterval = 10 * time.Millisecond
+)
+
+var (
+	keyCurrentTerm  = []byte("CurrentTerm")
+	keyLastVoteTerm = []byte("LastVoteTerm")
+	keyLastVoteCand = []byte("LastVoteCand")
+)
+
+// getRPCHeader returns an initialized RPCHeader struct for the given
+// Raft instance. This structure is sent along with RPC requests and
+// responses.
+func (r *Raft) getRPCHeader() RPCHeader {
+	return RPCHeader{
+		ProtocolVersion: r.conf.ProtocolVersion,
+	}
+}
