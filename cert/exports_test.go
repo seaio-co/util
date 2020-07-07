be154e787bc0611a57543dff1823c9ccf78441b6
august-star <august@hotmali.cn>
2020-07-07 09:56:47 +0800

    add exports test

---
+++ cert/exports_test.go
@@ -0,0 +1,10 @@
+// Copyright 2016 Canonical ltd.
+// Copyright 2016 Cloudbase solutions
+// Licensed under the LGPLv3, see LICENCE file for details.
+
+package cert
+
+var (
+	CertSubjAltName           = subjAltName
+	CertGetUPNExtenstionValue = getUPNExtensionValue
+)
